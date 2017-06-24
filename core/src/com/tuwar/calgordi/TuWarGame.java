00ea59b0d827d2fd252de59c550746f5636b02d2
Sergio Romero López <sorka1997@gmail.com>
2017-06-24 18:52:40 +0200

    Actualizacion botones

--- core/src/com/tuwar/calgordi/TuWarGame.java
+++ core/src/com/tuwar/calgordi/TuWarGame.java
@@ -3,9 +3,7 @@ package com.tuwar.calgordi;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.BitmapFontLoader;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -16,10 +14,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.Align;
-import javafx.scene.paint.Color;
-import jdk.nashorn.internal.runtime.Debug;
-import jdk.nashorn.internal.runtime.RecompilableScriptFunctionData;
-import sun.font.TrueTypeFont;
 
 
 public class TuWarGame extends ApplicationAdapter {
@@ -30,22 +24,29 @@ public class TuWarGame extends ApplicationAdapter {
 	private int ancho;
 	private int alto;
 
-	private BitmapFont fuente;
 	private Sprite fondoDerechaSprite ;
 	private Sprite fondoIzquierdaSprite ;
 
+	private BitmapFont fuenteTB;
 	private TextField nombreUsuario;
 	private TextField contraUsuario;
 	private TextField.TextFieldStyle textFieldStyle;
-	private Skin skin;
+	private Skin skinTF;
 
+	private BitmapFont fuenteCB;
 	private CheckBox recordarCuenta;
 	private CheckBox.CheckBoxStyle recordarCuentaStyle;
+	private Skin skinCB;
 
 	private TextButton iniciarSesion;
 	private TextButton crearCuenta;
 	private TextButton.TextButtonStyle textButtonStyle;
 	private TextureAtlas inicioAtlas;
+	private Skin skinTB;
+
+	private BitmapFont fuenteFP;
+	private TextButton.TextButtonStyle noSkin;
+	private TextButton forgetPass;
 	@Override
 	public void create () {
 		//En la partida añadir ortographic camera para que se renderize segun se mueva el jugador
@@ -62,7 +63,12 @@ public class TuWarGame extends ApplicationAdapter {
 		ancho = Gdx.graphics.getWidth();
 		alto = Gdx.graphics.getHeight();
 
+		BitmapFont fuente;
 		fuente = assets.get("data/fuentesTexto/MaiandraGD.fnt", BitmapFont.class);
+
+		fuenteTB = fuente;
+		fuenteCB = fuente;
+		fuenteFP= fuente;
 		inicioAtlas = assets.get("data/imagenes/inicio/inicio.pack",TextureAtlas.class);
 
 		fondoDerechaSprite = new Sprite(inicioAtlas.findRegion("FondoDerecha"));
@@ -73,12 +79,12 @@ public class TuWarGame extends ApplicationAdapter {
 
 		//Estilo del textField
 		textFieldStyle = new TextField.TextFieldStyle();
-		textFieldStyle.font = fuente;
-		skin = new Skin();
-		skin.addRegions(inicioAtlas);
+		textFieldStyle.font = fuenteFP;
+		skinTF = new Skin();
+		skinTF.addRegions(inicioAtlas);
 		textFieldStyle.messageFontColor = new com.badlogic.gdx.graphics.Color(1,1,1,0.5f);
 		textFieldStyle.fontColor= new com.badlogic.gdx.graphics.Color(1,1,1,0.6f);
-		textFieldStyle.background = skin.getDrawable("FieldText");
+		textFieldStyle.background = skinTF.getDrawable("FieldText");
 
 		//Texts fields
 		nombreUsuario = new TextField("", textFieldStyle);
@@ -108,25 +114,25 @@ public class TuWarGame extends ApplicationAdapter {
 
 		//CheckBox style
 		recordarCuentaStyle = new CheckBox.CheckBoxStyle();
-		recordarCuentaStyle.font = fuente;
+		recordarCuentaStyle.font = fuenteCB;
+		skinCB = new Skin();
+		skinCB.addRegions(inicioAtlas);
 		recordarCuentaStyle.fontColor = new com.badlogic.gdx.graphics.Color(0.5f,0.5f,1f,0.5f);
-		recordarCuentaStyle.checkboxOff = skin.getDrawable("CheckBox");
-		recordarCuentaStyle.checkboxOn = skin.getDrawable("CheckBoxAceptado");
+		recordarCuentaStyle.checkboxOff = skinCB.getDrawable("CheckBox");
+		recordarCuentaStyle.checkboxOn = skinCB.getDrawable("CheckBoxAceptado");
 
 		//CheckBox111111
 		recordarCuenta = new CheckBox("Recordar usuario.", recordarCuentaStyle);
-		recordarCuenta.getCells().get(0).width(5f);
-		recordarCuenta.getCells().get(0).height(5f);
 		recordarCuenta.setPosition(ancho * 3/5, alto *5/16);
-
-		recordarCuentaStyle.font.getData().setScale(anchoNU * 0.2f/256);
 
 		// Button style
 		textButtonStyle = new TextButton.TextButtonStyle();
-		textButtonStyle.font = fuente;
+		textButtonStyle.font = fuenteTB;
+		skinTB = new Skin();
+		skinTB.addRegions(inicioAtlas);
 		textButtonStyle.fontColor= new com.badlogic.gdx.graphics.Color(1,1,1,0.6f);
-		textButtonStyle.up = skin.getDrawable("buttonSkin");
-		textButtonStyle.down = skin.getDrawable("buttonSkin2");
+		textButtonStyle.up = skinTB.getDrawable("buttonSkin");
+		textButtonStyle.down = skinTB.getDrawable("buttonSkin2");
 
 		//Botones
 		iniciarSesion = new TextButton("INICIAR SESION", textButtonStyle);
@@ -137,13 +143,29 @@ public class TuWarGame extends ApplicationAdapter {
 		crearCuenta.setBounds(ancho * 3/5, alto /100, ancho * 2/5, alto / 5);
 		crearCuenta.align(Align.center);
 
-		textButtonStyle.font.getData().setScale(anchoNU * 0.55f/256);
+		//TextStyle del botton para recordar contra
+		noSkin = new TextButton.TextButtonStyle();
+		noSkin.font = fuenteFP;
+		noSkin.fontColor= new com.badlogic.gdx.graphics.Color(0.5f,0.5f,1f,0.6f);
+		noSkin.up = null;
+		noSkin.down = null;
+		noSkin.downFontColor = new com.badlogic.gdx.graphics.Color(0.2f,0.2f,1f,0.6f);
+
+		forgetPass = new TextButton("He olvidado mi contraseña", noSkin);
+		forgetPass.setBounds(ancho * 3/5, alto *5/16, ancho * 2/5, alto / 10);
+		forgetPass.align(Align.center);
+
+		fuenteTB.getData().setScale(anchoNU * 0.5f/256);
+		fuenteCB.getData().setScale(anchoNU * 0.3f/256);
+		fuenteFP.getData().setScale(anchoNU * 0.5f/256);
+
 
 		stage.addActor(nombreUsuario);
 		stage.addActor(contraUsuario);
 		stage.addActor(recordarCuenta);
 		stage.addActor(iniciarSesion);
 		stage.addActor(crearCuenta);
+		stage.addActor(forgetPass);
 	}
 
 
