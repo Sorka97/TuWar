4ccfde5accd441ab994c46ad07bf74894a07df03
Sergio Romero López <sorka1997@gmail.com>
2017-06-25 23:18:12 +0200

    Soy sergio

--- core/src/com/tuwar/calgordi/CrearTabla.java
+++ core/src/com/tuwar/calgordi/CrearTabla.java
@@ -1,14 +1,29 @@
 package com.tuwar.calgordi;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
 /**
  * Created by sergi_000 on 25/06/2017.
  */
 public class CrearTabla {
 
+    Table pantalla;
+    float ancho;
+    float alto;
 
 
 
 
     public void create(){
+        pantalla = new Table();
+        ancho = Gdx.graphics.getWidth();
+        alto = Gdx.graphics.getHeight();
+
+        pantalla.setBounds(0,0, ancho, alto);
+        //Distribucion en las dos partes
+        pantalla.add("parteIzquierda").setActorBounds(0,0,ancho * 3/5, alto);
+        pantalla.add("parteDerecha").setActorBounds(ancho * 3/5, 0, ancho * 2/5, alto);
 
 
     }
